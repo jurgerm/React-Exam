bfb6c89baed5d67260927a4cc7921dc60479db2a
Jurgita Germanavičienė <jurgita.germanaviciene@codeacademylt.onmicrosoft.com>
2022-01-26 19:09:47 +0200

    Added Footer

---
+++ react-exam/src/organisms/MyFooter.jsx
@@ -0,0 +1,8 @@
+import { MyFooterWrapper } from "../ui/MyFooterWrapper";
+export const MyFooter = () => {
+    return (
+        <MyFooterWrapper>
+            &copy; Jurgita Germanavičienė, 2022
+        </MyFooterWrapper>
+    );
+};
